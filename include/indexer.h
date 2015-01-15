d1f0826b59aaaa034282a5226f686fa5ab162050
Zarian Waheed <zarianwaheed@live.com>
2015-01-15 15:40:57 -0800

    Doing code formatting

--- include/indexer.h
+++ include/indexer.h
@@ -8,11 +8,12 @@ class IndexInfo;
 class Document;
 
 class Indexer {
-public:
-  virtual ~Indexer() {};
+ public:
+  virtual ~Indexer() {
+  }
   // This function validates that the document is valid for insert.
   // If this function returns OK status then the insert function call on the same document must succeed.
   virtual Status ValidateForInsert(const Document& document) = 0;
-  virtual void Insert(const Document& document) = 0; 
+  virtual void Insert(const Document& document) = 0;
 };
 }
--- include/internal/ewah_compressed_bitmap_indexer.h
+++ include/internal/ewah_compressed_bitmap_indexer.h
@@ -11,20 +11,21 @@ namespace jonoondb_api {
 class Document;
 class MamaJenniesBitmap;
 
-template <typename T>
+template<typename T>
 class EWAHCompressedBitmapIndexer final : public Indexer {
-public:
-  EWAHCompressedBitmapIndexer(const IndexInfo& indexInfo) : m_indexInfo(indexInfo) {}
-  ~EWAHCompressedBitmapIndexer() override {};
-  
+ public:
+  EWAHCompressedBitmapIndexer(const IndexInfo& indexInfo)
+      : m_indexInfo(indexInfo) {
+  }
+  ~EWAHCompressedBitmapIndexer() override {
+  }
   Status ValidateForInsert(const Document& document) override {
     return Status();
   }
-
   void Insert(const Document& document) override {
   }
-private:
+ private:
   IndexInfo m_indexInfo;
   std::map<T, std::shared_ptr<MamaJenniesBitmap>> m_compressedBitmaps;
 };
-} // namespace jonoondb_api
+}  // namespace jonoondb_api
