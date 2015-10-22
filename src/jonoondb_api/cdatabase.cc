cb3b1b5b2677a00dba333adf5e6707274eef07cd
zarianw <zarianwaheed@live.com>
2015-10-22 16:37:16 -0700

    saving translate exception function.

--- src/jonoondb_api/cdatabase.cc
+++ src/jonoondb_api/cdatabase.cc
@@ -1,15 +1,77 @@
 #include "cdatabase.h"
 #include "status.h"
 #include "options.h"
+#include "jonoondb_exceptions.h"
 
 using namespace jonoondb_api;
 
-extern "C" {
+//
+// Error Handling
+//
+// Returns true if fn executed without throwing an error, false otherwise.
+// If calling fn threw an error, capture it in *out_error.
+template<typename Fn>
+bool translateExceptions(Fn&& fn, status_ptr& sts) {
+  bool retVal = false;
+  try {
+    fn();
+    retVal = true;
+  } catch (const InvalidArgumentException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const MissingDatabaseFileException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const MissingDatabaseFolderException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const OutOfMemoryException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const DuplicateKeyException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const CollectionAlreadyExistException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const IndexAlreadyExistException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const CollectionNotFoundException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const SchemaParseException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const IndexOutOfBoundException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const SQLException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const JonoonDBException& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (const std::exception& ex) {
+    sts = new status(kStatusInvalidArgumentCode, ex.what(), ex.GetSourceFileName(),
+      ex.GetFunctionName(), ex.GetLineNumber());
+  } catch (...) {
+    sts = new status(kStatusInvalidArgumentCode, "Unknown Error.", "", "", "");
+  }
 
+  return retVal;
+}
+
+extern "C" {
 //
 // Status
 //
 struct status {
+  status(std::size_t code, const char* message, const char* srcFileName,
+    const char* funcName, std::size_t lineNum) :
+    impl(code, message, srcFileName, funcName, lineNum) {
+  }
+
   Status impl;
 };
 
