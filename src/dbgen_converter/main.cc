62d2bf9d49e220e026d56b7289e56151b6409360
usmanw <usman.waheed@jonoondb.com>
2016-02-15 02:44:37 +0300

    Fixed scope, and program is working now

--- src/dbgen_converter/main.cc
+++ src/dbgen_converter/main.cc
@@ -4,6 +4,9 @@
 #include "C:\code\jonoondb\test\schemas\flatbuffers\region_generated.h"
 #include "C:\code\jonoondb\test\schemas\flatbuffers\nation_generated.h"
 #include "C:\code\jonoondb\test\schemas\flatbuffers\lineitem_generated.h"
+#include "C:\code\jonoondb\test\schemas\flatbuffers\partsupp_generated.h"
+#include "C:\code\jonoondb\test\schemas\flatbuffers\part_generated.h"
+#include "C:\code\jonoondb\test\schemas\flatbuffers\orders_generated.h"
 #include "flatbuffers/flatbuffers.h"
 
 using namespace dbgen_loader;
@@ -61,27 +64,40 @@ int main(int argc, char **argv) {
     }
   }
 
-  {
-    // Nation
-    string filePath("C:/code/tpch_2_14_3/dbgen/nation.tbl");
-    ifstream file(filePath.c_str());
-    if (!file.is_open()) return 1;
-
-    vector<string> tokens;
-    string line;
-    char_separator<char> sep("|");
-
-    while (getline(file, line))
     {
-      tokenizer<char_separator<char>> tok(line, sep);
-      tokens.assign(tok.begin(), tok.end());
+      // Nation
+      string filePath("C:/code/tpch_2_14_3/dbgen/nation.tbl");
+      ifstream file(filePath.c_str());
+      if (!file.is_open()) return 1;
 
-      FlatBufferBuilder fbb;
-      auto N_NAME = fbb.CreateString(tokens[1]);
-      auto N_COMMENT = fbb.CreateString(tokens[3]);
-      auto nationKey = stoi(tokens[0]);
-      auto regionKey = stoi(tokens[2]);
-      auto nation = CreateNATION(fbb, nationKey, N_NAME, regionKey, N_COMMENT);
+      vector<string> tokens;
+      string line;
+      char_separator<char> sep("|");
+
+      std::remove("nation.fb");
+
+      ofstream outputFile("nation.fb", ios::binary);
+
+      if (!outputFile.is_open()) return 1;
+
+      while (getline(file, line))
+      {
+        tokenizer<char_separator<char>> tok(line, sep);
+        tokens.assign(tok.begin(), tok.end());
+
+        FlatBufferBuilder fbb;
+        auto N_NAME = fbb.CreateString(tokens[1]);
+        auto N_COMMENT = fbb.CreateString(tokens[3]);
+        auto nationKey = stoi(tokens[0]);
+        auto regionKey = stoi(tokens[2]);
+        auto nation = CreateNATION(fbb, nationKey, N_NAME, regionKey, N_COMMENT);
+        fbb.Finish(nation);
+
+        int32_t size = static_cast<int32_t>(fbb.GetSize());
+        outputFile.write(reinterpret_cast<char*>(&size), sizeof(int32_t));
+        outputFile.write(reinterpret_cast<char*>(fbb.GetBufferPointer()), size);
+
+      }
     }
     {
       string filePath("C:/code/tpch_2_14_3/dbgen/lineitem.tbl");
@@ -91,6 +107,11 @@ int main(int argc, char **argv) {
       vector<string> tokens;
       string line;
       char_separator<char> sep("|");
+      std::remove("lineitem.fb");
+
+      ofstream outputFile("lineitem.fb", ios::binary);
+
+      if (!outputFile.is_open()) return 1;
 
       while (getline(file, line))
       {
@@ -98,23 +119,142 @@ int main(int argc, char **argv) {
         tokens.assign(tok.begin(), tok.end());
 
         FlatBufferBuilder fbb;
-        auto l_returnFlag = fbb.CreateString(tokens[8]);
-        auto l_lineStatus = fbb.CreateString(tokens[9]);
-        auto l_shipDate = fbb.CreateString(tokens[10]);
-        auto l_commitDate = fbb.CreateString(tokens[11]);
-        auto l_receiptDate = fbb.CreateString(tokens[12]);
-        auto l_shipInstruct = fbb.CreateString(tokens[13]);
-        auto l_shipMode = fbb.CreateString(tokens[14]);
-        auto l_comment = fbb.CreateString(tokens[15]);
-        auto l_orderKey = stoi(tokens[0]);
-        auto l_partKey = stoi(tokens[1]);
-        auto l_suppKey = stoi(tokens[2]);
-        auto l_lineNumber = stoi(tokens[3]);
-        auto l_quantity = stod(tokens[4]);
-        auto l_extendedPrice = stod(tokens[5]);
-        auto l_discount = stod(tokens[6]);
-        auto l_tax = stod(tokens[7]);
-        auto lineItem = CreateLINEITEM(fbb, l_orderKey, l_partKey, l_suppKey, l_lineNumber, l_quantity, l_extendedPrice, l_discount);
+        auto returnFlag = fbb.CreateString(tokens[8]);
+        auto lineStatus = fbb.CreateString(tokens[9]);
+        auto shipDate = fbb.CreateString(tokens[10]);
+        auto commitDate = fbb.CreateString(tokens[11]);
+        auto receiptDate = fbb.CreateString(tokens[12]);
+        auto shipInstruct = fbb.CreateString(tokens[13]);
+        auto shipMode = fbb.CreateString(tokens[14]);
+        auto comment = fbb.CreateString(tokens[15]);
+        auto orderKey = stoi(tokens[0]);
+        auto partKey = stoi(tokens[1]);
+        auto suppKey = stoi(tokens[2]);
+        auto lineNumber = stoi(tokens[3]);
+        auto quantity = stod(tokens[4]);
+        auto extendedPrice = stod(tokens[5]);
+        auto discount = stod(tokens[6]);
+        auto tax = stod(tokens[7]);
+        auto lineItem = CreateLINEITEM(fbb, orderKey, partKey, suppKey, lineNumber, quantity, extendedPrice,
+          discount, tax, returnFlag, lineStatus, shipDate, commitDate, receiptDate, shipInstruct, shipMode, comment);
+
+        fbb.Finish(lineItem);
+        int32_t size = static_cast<int32_t>(fbb.GetSize());
+        outputFile.write(reinterpret_cast<char*>(&size), sizeof(int32_t));
+        outputFile.write(reinterpret_cast<char*>(fbb.GetBufferPointer()), size);
+
+      }
+
+    }
+    {
+      string filePath("C:/code/tpch_2_14_3/dbgen/partsupp.tbl");
+
+      ifstream file(filePath.c_str());
+      if (!file.is_open()) return 1;
+
+      vector<string> tokens;
+      string line;
+      char_separator<char> sep("|");
+
+      std::remove("partsupp.fb");
+      ofstream outputFile("partsupp.fb", ios::binary);
+      if (!outputFile.is_open()) return 1;
+
+      while (getline(file, line))
+      {
+        tokenizer<char_separator<char>> tok(line, sep);
+        tokens.assign(tok.begin(), tok.end());
+
+        FlatBufferBuilder fbb;
+        auto comment = fbb.CreateString(tokens[4]);
+        auto partKey = stoi(tokens[0]);
+        auto suppKey = stoi(tokens[1]);
+        auto availQty = stoi(tokens[2]);
+        auto supplyCost = stod(tokens[3]);
+        auto partSupp = CreatePARTSUPP(fbb, partKey, suppKey, availQty, supplyCost, comment);
+        fbb.Finish(partSupp);
       }
     }
+
+    {
+      string filePath("C:/code/tpch_2_14_3/dbgen/part.tbl");
+
+      ifstream file(filePath.c_str());
+      if (!file.is_open()) return 1;
+
+      vector<string> tokens;
+      string line;
+      char_separator<char> sep("|");
+
+      std::remove("part.fb");
+      ofstream outputFile("part.fb", ios::binary);
+      if (!outputFile.is_open()) return 1;
+
+      while (getline(file, line))
+      {
+        tokenizer<char_separator<char>> tok(line, sep);
+        tokens.assign(tok.begin(), tok.end());
+
+        FlatBufferBuilder fbb;
+        auto name = fbb.CreateString(tokens[1]);
+        auto mfgr = fbb.CreateString(tokens[2]);
+        auto brand = fbb.CreateString(tokens[3]);
+        auto type = fbb.CreateString(tokens[4]);
+        auto container = fbb.CreateString(tokens[6]);
+        auto comment = fbb.CreateString(tokens[8]);
+        auto partKey = stoi(tokens[0]);
+        auto partSize = stoi(tokens[5]);
+        auto retailPrice = stod(tokens[7]);
+        auto part = CreatePART(fbb, partKey, name, mfgr, brand, type, partSize, container, retailPrice, comment);
+        fbb.Finish(part);
+        int32_t size = static_cast<int32_t>(fbb.GetSize()); // get size of the flat buffer
+        outputFile.write(reinterpret_cast<char*>(&size), sizeof(int32_t));
+        outputFile.write(reinterpret_cast<char*>(fbb.GetBufferPointer()), size);
+
+      }
+    }
+
+    {
+    string filePath("C:/code/tpch_2_14_3/dbgen/orders.tbl");
+    ifstream file(filePath.c_str());
+    if (!file.is_open()) return 1;
+
+    vector<string> tokens;
+    string line;
+    char_separator<char> sep("|");
+
+    std::remove("orders.fb");
+
+    ofstream outputFile("orders.fb", ios::binary);
+
+    if (!outputFile.is_open()) return 1;
+
+    while (getline(file, line))
+    {
+      tokenizer<char_separator<char>> tok(line, sep);
+      tokens.assign(tok.begin(), tok.end());
+
+      FlatBufferBuilder fbb;
+
+      auto orderStatus = fbb.CreateString(tokens[2]);
+      auto orderDate = fbb.CreateString(tokens[4]);
+      auto orderPriorty = fbb.CreateString(tokens[5]);
+      auto clerk = fbb.CreateString(tokens[6]);
+      auto comment = fbb.CreateString(tokens[8]);
+      auto orderKey = stoi(tokens[0]);
+      auto custKey = stoi(tokens[1]);
+      auto totalPrice = stod(tokens[3]);
+      auto shipPriority = stoi(tokens[7]);
+
+      auto orders = CreateORDERS(fbb, orderKey, custKey, orderStatus, totalPrice, orderDate, orderPriorty, clerk, shipPriority, comment);
+      fbb.Finish(orders);
+
+      int32_t size = static_cast<int32_t>(fbb.GetSize());
+      outputFile.write(reinterpret_cast<char*>(&size), sizeof(int32_t));
+      outputFile.write(reinterpret_cast<char*>(fbb.GetBufferPointer()), size);
+
+    }
+
+
+  }
 }
