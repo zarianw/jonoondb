5ba42323f1f9aef87b4dcf113e9afc15ccc1b0c4
Zarian Waheed <zarianwaheed@live.com>
2016-05-10 17:22:01 -0700

    Added code to get process memory usage on linux.

--- include/jonoondb_api/proc_utils.h
+++ include/jonoondb_api/proc_utils.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <memory>
+#include <string>
+#include <fstream>
+#include <cstdlib>
 #include "jonoondb_api/jonoondb_exceptions.h"
 #include "jonoondb_api/exception_utils.h"
 
@@ -9,6 +12,8 @@
 #include <Psapi.h>
 #else
 // Linux code goes here
+#include <sys/types.h>
+#include <unistd.h>
 #endif
 
 namespace jonoondb_api {
@@ -33,7 +38,26 @@ public:
 #else
   // Linux code goes here
   static void GetProcessMemoryStats(ProcessMemStat& stat) {
+    static auto pageSize = getpagesize();
     stat.MemoryUsedInBytes = 0;
+    auto pid = getpid();
+    std::string fileName = "/proc/";
+    fileName.append(std::to_string(pid));
+    fileName.append("/statm");
+    std::ifstream ifs(fileName);
+    std::string line;
+    std::getline(ifs, line);
+    if(line.size() == 0) {
+      //throw exception
+    }
+
+    char* curr = nullptr;
+    // read size - total program size
+    auto size = strtoull(line.c_str(), &curr, 10);
+    // read resident - resident set size
+    auto resident = strtoull(curr, &curr, 10);
+
+    stat.MemoryUsedInBytes = resident * pageSize;
   }
 #endif 
 };
