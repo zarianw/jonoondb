35948f2194d37ef82d55caa8a9994e3e9e932ee8
zwaheed <zarianwaheed@live.com>
2016-06-14 02:47:52 -0700

    Enabling caching in travis

---
+++ scripts/install_boost_1.60.0.sh
@@ -0,0 +1,8 @@
+set -x
+set -e
+if [ ! -e boost_1_60_0 ]; then
+  wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz
+  tar xvzf boost_1_60_0.tar.gz > boost.out
+  $(cd boost_1_60_0 && ./bootstrap.sh --with-libraries=filesystem,system,thread,chrono,program_options >> boost.out && ./b2 install address-model=64 --prefix=64bit >> boost.out)
+fi
+
