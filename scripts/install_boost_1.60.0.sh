759225ad5142c5b52e8182708fbde81cb148cc1b
zarianw <zarianwaheed@live.com>
2016-10-02 17:58:32 -0700

    Clearing old boost folder.

--- scripts/install_boost_1.60.0.sh
+++ scripts/install_boost_1.60.0.sh
@@ -1,5 +1,6 @@
 set -x
 set -e
+rm -rf boost_1_60_0
 if [ ! -e boost_1_60_0 ]; then
   wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz
   tar xzf boost_1_60_0.tar.gz > boost.out
